0270c13a2a779fc93dd095f09f0ce6d8b4eec02e
Pedro <pelg.pedro@gmail.com>
2026-03-30 21:36:27 -0300

    Repository

---
+++ src/main/java/backend_receitas/repository/ReceitaRepository.java
@@ -0,0 +1,10 @@
+package backend_receitas.repository;
+
+import backend_receitas.model.Receita;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReceitaRepository extends JpaRepository<Receita, Long> {
+
+}
